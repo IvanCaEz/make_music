d882a9b2cbba4b31305ad20df46a9246beadf4c8
Iván Martínez Cañero <125135728+IvanCaEz@users.noreply.github.com>
2023-11-15 16:58:50 +0100

    change gestures

--- main.py
+++ main.py
@@ -6,15 +6,15 @@ input.on_button_pressed(Button.A, on_button_pressed_a)
 
 def on_button_pressed_b():
     music.stop_all_sounds()
-    music.play(music.string_playable("G B C5 F D B A C ", 120),
+    music.play(music.string_playable("G B A G C5 B A B ", 120),
         music.PlaybackMode.LOOPING_IN_BACKGROUND)
 input.on_button_pressed(Button.B, on_button_pressed_b)
 
 def on_forever():
-    if input.is_gesture(Gesture.TILT_LEFT):
+    if input.is_gesture(Gesture.LOGO_DOWN):
         music.set_tempo(80)
         basic.show_number(music.tempo())
-    elif input.is_gesture(Gesture.TILT_RIGHT):
+    elif input.is_gesture(Gesture.LOGO_UP):
         music.set_tempo(160)
         basic.show_number(music.tempo())
 basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -1,17 +1,18 @@
-input.onButtonPressed(Button.A, function () {
+input.onButtonPressed(Button.A, function on_button_pressed_a() {
     music.stopAllSounds()
     music.play(music.stringPlayable("A F E F D G E F ", 120), music.PlaybackMode.LoopingInBackground)
 })
-input.onButtonPressed(Button.B, function () {
+input.onButtonPressed(Button.B, function on_button_pressed_b() {
     music.stopAllSounds()
     music.play(music.stringPlayable("G B A G C5 B A B ", 120), music.PlaybackMode.LoopingInBackground)
 })
-basic.forever(function () {
-    if (input.isGesture(Gesture.TiltLeft)) {
+basic.forever(function on_forever() {
+    if (input.isGesture(Gesture.LogoDown)) {
         music.setTempo(80)
         basic.showNumber(music.tempo())
-    } else if (input.isGesture(Gesture.TiltRight)) {
+    } else if (input.isGesture(Gesture.LogoUp)) {
         music.setTempo(160)
         basic.showNumber(music.tempo())
     }
+    
 })
